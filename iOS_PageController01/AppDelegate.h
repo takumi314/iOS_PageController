43802259ac2f2d04d0bcc85159c98e8e4dcb2b3c
takumi314 <isospin_symmetry@me.com>
2016-05-03 14:01:05 +0900

    Initial Commit

---
+++ iOS_PageController01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_PageController01
+//
+//  Created by NishiokaKohei on 2016/05/03.
+//  Copyright © 2016年 Kohei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_PageController01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_PageController01
+//
+//  Created by NishiokaKohei on 2016/05/03.
+//  Copyright © 2016年 Kohei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
