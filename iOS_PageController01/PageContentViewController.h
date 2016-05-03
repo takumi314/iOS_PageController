2fa2985164a0ccadcc48c532900f04f9c41938ce
takumi314 <isospin_symmetry@me.com>
2016-05-03 15:52:19 +0900

    Control and drag from the image view to the PageContentViewController.h and create an IBOutlet. Set the name as imageViewOnTheBackground for the image view. For the label, set the name of the outlet as titleLabel.

---
+++ iOS_PageController01/PageContentViewController.h
@@ -0,0 +1,16 @@
+//
+//  PageContentViewController.h
+//  iOS_PageController01
+//
+//  Created by NishiokaKohei on 2016/05/03.
+//  Copyright © 2016年 Kohei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageContentViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *imageOnTheBackground;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+
+@end
